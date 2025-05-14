26c8539ca59dbf72ad340bddd63472f0e8e9039e
Spencer Childress <spencer.c.childress@gmail.com>
2025-05-14 17:39:21 -0400

    fix #31

---
+++ R/CalculateChange.R
@@ -0,0 +1,98 @@
+#' Calculate Change
+#'
+#' Calculate the change from the previous snapshot date.
+#'
+#' @param dfResults `data.frame` A stacked summary of analysis pipeline output.
+#'   Created by passing a list of results returned by [Summarize()] to
+#'   [BindResults()]. Expected columns: `GroupID`, `GroupLevel`, `Numerator`,
+#'   `Denominator`, `Metric`, `Score`, `Flag`, `MetricID`, `StudyID`,
+#'   `SnapshotDate`.
+#' @param strIDColumns `character` A vector of column names with which to group
+#'   results. Default:
+#'   - StudyID
+#'   - GroupLevel
+#'   - GroupID
+#'   - MetricID
+#' @param strSnapshotDateColumn `character` The name of the column containing the
+#'   snapshot date. Default: `SnapshotDate`.
+#' @param strMetricColumns `character` A vector of numeric column names with which calculate change from
+#'   previous snapshot. Default:
+#'   - Numerator
+#'   - Denominator
+#'   - Metric
+#'   - Score
+#'   - Flag
+#'
+#' @return `data.frame` A transposed table of results with a column for each attribute, its value,
+#'   and the change from the previous snapshot.
+#'
+#' @example
+#' dfChChChChanges <- CalculateChanges(gsm.core::reportingResults)
+#'
+#' @export
+
+CalculateChange <- function(
+    dfResults,
+    strIDColumns = c(
+        "StudyID",
+        "GroupLevel",
+        "GroupID",
+        "MetricID"
+    ),
+    strSnapshotDateColumn = "SnapshotDate",
+    strMetricColumns = c(
+        "Numerator",
+        "Denominator",
+        "Metric",
+        "Score",
+        "Flag"
+    )
+) {
+    stop_if(
+        cnd = !is.data.frame(dfResults),
+        message = "`dfResults` must be a data frame."
+    )
+
+    # Check that all columns exist in [ dfResults ].
+    for (strColumn in c(
+        strIDColumns,
+        strSnapshotDateColumn,
+        strMetricColumns
+    )) {
+        stop_if(
+            cnd = !strColumn %in% colnames(dfResults),
+            message = glue::glue("`{strColumn} not found in `dfResults`.")
+        )
+    }
+
+    # Calculate change from previous snapshot.
+    dfChanges <- dfResults %>%
+        tidyr::pivot_longer(
+            cols = all_of(strMetricColumns),
+            names_to = "Param",
+            values_to = "Value"
+        ) %>%
+        dplyr::mutate(
+            Param = factor(Param, levels = strMetricColumns)
+        ) %>%
+        dplyr::group_by(across(all_of(c(
+            strIDColumns,
+            'Param'
+        )))) %>%
+        dplyr::arrange(
+            strSnapshotDateColumn,
+            .by_group = TRUE
+        ) %>%
+        dplyr::mutate(
+            Change = Value - dplyr::lag(Value),
+            PercentChange = Change / dplyr::lag(Value) * 100
+        ) %>%
+        dplyr::ungroup() %>%
+        dplyr::arrange(across(all_of(c(
+            strIDColumns,
+            'Param',
+            strSnapshotDateColumn
+        ))))
+
+    return(dfChanges)
+}
---
+++ tests/testthat/test-CalculateChange.R
@@ -0,0 +1,145 @@
+test_that(
+    '[ CalculateChange ] correctly calculates change from the previous snapshot.',
+    {
+        dfResults <- data.frame(
+            StudyID = c(1, 1, 1, 2, 2, 2),
+            GroupLevel = c("A", "A", "A", "B", "B", "B"),
+            GroupID = c(1, 1, 1, 2, 2, 2),
+            MetricID = c("Metric A", "Metric A", "Metric A", "Metric B", "Metric B", "Metric B"),
+            SnapshotDate = as.Date(c("2023-01-01", "2023-02-01", "2023-03-01", "2023-01-01", "2023-02-01", "2023-03-01")),
+            Numerator = c(10, 20, 30, 5, 10, 15),
+            Denominator = c(100, 200, 300, 50, 100, 150),
+            Metric = c(.1, .1, .1, .1, .1, .1),
+            Score = c(0.1, 0.2, 0.3, 0.05, 0.1, 0.15),
+            Flag = c(-2, -1, 0, 0, 1, 2)
+        )
+
+        strIDColumns <- c(
+            "StudyID",
+            "GroupLevel",
+            "GroupID",
+            "MetricID"
+        )
+
+        strSnapshotDateColumn <- "SnapshotDate"
+
+        strMetricColumns <- c(
+            "Numerator",
+            "Denominator",
+            "Metric",
+            "Score",
+            "Flag"
+        )
+
+        # [ CalculateChange ] returns a transposed data frame with one record per combination of the
+        # following columns, where [ Param ] is the name of the column and [ Value ] is the value of
+        # that column:
+        # - StudyID
+        # - GroupLevel
+        # - GroupID
+        # - MetricID
+        # - SnapshotDate
+        # - Param
+        dfChanges <- CalculateChange(
+            dfResults = dfResults
+        )
+
+        # Check that the output is a data frame.
+        expect_true(is.data.frame(dfChanges))
+
+        # Check that the output has the expected number of rows.
+        expect_equal(
+            nrow(dfChanges),
+            nrow(dfResults) * length(strMetricColumns)
+        )
+
+        # Check that the output has the expected columns.
+        expect_true(all(c(
+            strIDColumns,
+            strSnapshotDateColumn,
+            "Param",
+            "Value",
+            "Change",
+            "PercentChange"
+        ) %in% colnames(dfChanges)))
+
+        # Check that the output has the expected column names.
+        expect_equal(
+            colnames(dfChanges),
+            c(
+                strIDColumns,
+                strSnapshotDateColumn,
+                "Param",
+                "Value",
+                "Change",
+                "PercentChange"
+            )
+        )
+
+        # Check that the output has the expected values.
+        expect_equal(
+            dfChanges$Value[dfChanges$Param == "Numerator"],
+            c(10, 20, 30, 5, 10, 15)
+        )
+        expect_equal(
+            dfChanges$Value[dfChanges$Param == "Denominator"],
+            c(100, 200, 300, 50, 100, 150)
+        )
+        expect_equal(
+            dfChanges$Value[dfChanges$Param == "Metric"],
+            c(.1, .1, .1, .1, .1, .1)
+        )
+        expect_equal(
+            dfChanges$Value[dfChanges$Param == "Score"],
+            c(0.1, 0.2, 0.3, 0.05, 0.1, 0.15)
+        )
+        expect_equal(
+            dfChanges$Value[dfChanges$Param == "Flag"],
+            c(-2, -1, 0, 0, 1, 2)
+        )
+
+        # Check that the output has the expected change values.
+        expect_equal(
+            dfChanges$Change[dfChanges$Param == "Numerator"],
+            c(NA, 10, 10, NA, 5, 5)
+        )
+        expect_equal(
+            dfChanges$Change[dfChanges$Param == "Denominator"],
+            c(NA, 100, 100, NA, 50, 50)
+        )
+        expect_equal(
+            dfChanges$Change[dfChanges$Param == "Metric"],
+            c(NA, 0, 0, NA, 0, 0)
+        )
+        expect_equal(
+            dfChanges$Change[dfChanges$Param == "Score"],
+            c(NA, 0.1, 0.1, NA, 0.05, 0.05)
+        )
+        expect_equal(
+            dfChanges$Change[dfChanges$Param == "Flag"],
+            c(NA, 1, 1, NA, 1, 1)
+        )
+
+        # Check that the output has the expected percent change values.
+        expect_equal(
+            dfChanges$PercentChange[dfChanges$Param == "Numerator"],
+            c(NA, 100, 50, NA, 100, 50)
+        )
+        expect_equal(
+            dfChanges$PercentChange[dfChanges$Param == "Denominator"],
+            c(NA, 100, 50, NA, 100, 50)
+        )
+        expect_equal(
+            dfChanges$PercentChange[dfChanges$Param == "Metric"],
+            c(NA, 0, 0, NA, 0, 0)
+        )
+        expect_equal(
+            dfChanges$PercentChange[dfChanges$Param == "Score"],
+            c(NA, 100, 50, NA, 100, 50)
+        )
+        expect_equal(
+            dfChanges$PercentChange[dfChanges$Param == "Flag"],
+            c(NA, -50, -100, NA, Inf, 100)
+        )
+    }
+)
