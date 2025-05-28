5f7cd135a6daa981ff9854ad1519d6a77c6d50f8
Laura Maxwell <laura.maxwell@atorusresearch.com>
2025-05-28 15:46:27 -0400

    styler and description update

--- tests/testthat/test-CalculateChange.R
+++ tests/testthat/test-CalculateChange.R
@@ -44,107 +44,106 @@ strMetricColumns <- c(
 )
 
 test_that(
-    '[ CalculateChange ] correctly calculates change from the previous snapshot.',
-    {
+  "[ CalculateChange ] correctly calculates change from the previous snapshot.",
+  {
+    # [ CalculateChange ] returns a data frame with one record per combination of the
+    # following columns
+    # - StudyID
+    # - GroupLevel
+    # - GroupID
+    # - MetricID
+    # - SnapshotDate
+    dfChanges <- CalculateChange(
+      dfResults = dfResults,
+      dfResultsLongitudinal = dfPrevious
+    )
 
-        # [ CalculateChange ] returns a data frame with one record per combination of the
-        # following columns
-        # - StudyID
-        # - GroupLevel
-        # - GroupID
-        # - MetricID
-        # - SnapshotDate
-        dfChanges <- CalculateChange(
-            dfResults = dfResults,
-            dfResultsLongitudinal = dfPrevious
-        )
+    # Check that the output is a data frame.
+    expect_true(is.data.frame(dfChanges))
 
-        # Check that the output is a data frame.
-        expect_true(is.data.frame(dfChanges))
+    # Check that the output has the expected number of rows.
+    expect_equal(
+      nrow(dfChanges),
+      sum(nrow(dfResults), nrow(dfPrevious))
+    )
 
-        # Check that the output has the expected number of rows.
-        expect_equal(
-            nrow(dfChanges),
-            sum(nrow(dfResults), nrow(dfPrevious))
-        )
-
-        # Check that the output has the expected columns.
-        expect_true(all(c(
-            strIDColumns,
-            strSnapshotDateColumn,
-            "PrevSnapshotDate",
-            paste(strMetricColumns,"Previous", sep = "_"),
-            paste(strMetricColumns,"Change", sep = "_"),
-            paste(strMetricColumns,"PercentChange", sep = "_")
-        ) %in% colnames(dfChanges)))
+    # Check that the output has the expected columns.
+    expect_true(all(c(
+      strIDColumns,
+      strSnapshotDateColumn,
+      "PrevSnapshotDate",
+      paste(strMetricColumns, "Previous", sep = "_"),
+      paste(strMetricColumns, "Change", sep = "_"),
+      paste(strMetricColumns, "PercentChange", sep = "_")
+    ) %in% colnames(dfChanges)))
 
 
-        # Check that the output has the expected values.
-        expect_equal(
-            dfChanges$Numerator,
-            c(10, 10, 25, 20, 40, 30,  5,  5,  5, 10, 15, 15)
-        )
-        expect_equal(
-            dfChanges$Denominator,
-            c(100, 100, 250, 200, 400, 300,  50,  50,  50, 100, 150, 150)
-        )
-        expect_equal(
-            dfChanges$Metric,
-            c(.1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1)
-        )
-        expect_equal(
-            dfChanges$Score,
-            c(0.10, 0.10, 0.20, 0.20, 0.30, 0.30, 0.05, 0.05, 0.10, 0.10, 0.15, 0.15)
-        )
-        expect_equal(
-            dfChanges$Flag,
-            c(-2, -2,  0, -1,  1,  0,  0,  0,  0,  1,  2,  2)
-        )
+    # Check that the output has the expected values.
+    expect_equal(
+      dfChanges$Numerator,
+      c(10, 10, 25, 20, 40, 30, 5, 5, 5, 10, 15, 15)
+    )
+    expect_equal(
+      dfChanges$Denominator,
+      c(100, 100, 250, 200, 400, 300, 50, 50, 50, 100, 150, 150)
+    )
+    expect_equal(
+      dfChanges$Metric,
+      c(.1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1, .1)
+    )
+    expect_equal(
+      dfChanges$Score,
+      c(0.10, 0.10, 0.20, 0.20, 0.30, 0.30, 0.05, 0.05, 0.10, 0.10, 0.15, 0.15)
+    )
+    expect_equal(
+      dfChanges$Flag,
+      c(-2, -2, 0, -1, 1, 0, 0, 0, 0, 1, 2, 2)
+    )
 
-        # Check that the output has the expected change values.
-        expect_equal(
-            dfChanges$Numerator_Change,
-            c(0, NA,  5, NA, 10, NA,  0, NA, -5, NA,  0, NA)
-        )
-        expect_equal(
-            dfChanges$Denominator_Change,
-            c(0,  NA,  50,  NA, 100,  NA,   0,  NA, -50,  NA,   0,  NA)
-        )
-        expect_equal(
-            dfChanges$Metric_Change,
-            c( 0, NA,  0, NA,  0, NA,  0, NA, 0, NA, 0, NA)
-        )
-        expect_equal(
-            dfChanges$Score_Change,
-            c( 0, NA,  0, NA,  0, NA,  0, NA, 0, NA, 0, NA)
-        )
-        expect_equal(
-            dfChanges$Flag_Change,
-            c( 0, NA,  1, NA,  1, NA,  0, NA, -1, NA,  0, NA)
-        )
+    # Check that the output has the expected change values.
+    expect_equal(
+      dfChanges$Numerator_Change,
+      c(0, NA, 5, NA, 10, NA, 0, NA, -5, NA, 0, NA)
+    )
+    expect_equal(
+      dfChanges$Denominator_Change,
+      c(0, NA, 50, NA, 100, NA, 0, NA, -50, NA, 0, NA)
+    )
+    expect_equal(
+      dfChanges$Metric_Change,
+      c(0, NA, 0, NA, 0, NA, 0, NA, 0, NA, 0, NA)
+    )
+    expect_equal(
+      dfChanges$Score_Change,
+      c(0, NA, 0, NA, 0, NA, 0, NA, 0, NA, 0, NA)
+    )
+    expect_equal(
+      dfChanges$Flag_Change,
+      c(0, NA, 1, NA, 1, NA, 0, NA, -1, NA, 0, NA)
+    )
 
-        # Check that the output has the expected percent change values.
-        expect_equal(
-            dfChanges$Numerator_PercentChange,
-            c(0,NA, 25, NA, 100/3, NA, 0, NA, -50, NA, 0, NA)
-        )
-        expect_equal(
-            dfChanges$Denominator_PercentChange,
-            c(0,NA, 25, NA, 100/3, NA, 0, NA, -50, NA, 0, NA)
-        )
-        expect_equal(
-            dfChanges$Metric_PercentChange,
-            c( 0, NA,  0, NA,  0, NA,  0, NA, 0, NA, 0, NA)
-        )
-        expect_equal(
-            dfChanges$Score_PercentChange,
-            c( 0, NA,  0, NA,  0, NA,  0, NA, 0, NA, 0, NA)
-        )
-        expect_equal(
-            dfChanges$Flag_PercentChange,
-            c(0,   NA, -100,   NA,  Inf,   NA,  NaN,   NA, -100,   NA,    0,  NA)
-        )
-    }
+    # Check that the output has the expected percent change values.
+    expect_equal(
+      dfChanges$Numerator_PercentChange,
+      c(0, NA, 25, NA, 100 / 3, NA, 0, NA, -50, NA, 0, NA)
+    )
+    expect_equal(
+      dfChanges$Denominator_PercentChange,
+      c(0, NA, 25, NA, 100 / 3, NA, 0, NA, -50, NA, 0, NA)
+    )
+    expect_equal(
+      dfChanges$Metric_PercentChange,
+      c(0, NA, 0, NA, 0, NA, 0, NA, 0, NA, 0, NA)
+    )
+    expect_equal(
+      dfChanges$Score_PercentChange,
+      c(0, NA, 0, NA, 0, NA, 0, NA, 0, NA, 0, NA)
+    )
+    expect_equal(
+      dfChanges$Flag_PercentChange,
+      c(0, NA, -100, NA, Inf, NA, NaN, NA, -100, NA, 0, NA)
+    )
+  }
 )
 
 # test_that(
@@ -216,7 +215,7 @@ test_that(
 # )
 
 test_that(
-  '[ CalculateChange ] returns message and dfResults unchanged if dfPrevious not supplied.',
+  "[ CalculateChange ] returns message and dfResults unchanged if dfPrevious not supplied.",
   {
     dfChanges <- CalculateChange(
       dfResults = dfResults
@@ -224,5 +223,7 @@ test_that(
     expect_equal(dfChanges, dfResults)
 
     expect_message(CalculateChange(
-      dfResults = dfResults), regexp = "dfResults will be returned unchanged")
-})
+      dfResults = dfResults
+    ), regexp = "dfResults will be returned unchanged")
+  }
+)
