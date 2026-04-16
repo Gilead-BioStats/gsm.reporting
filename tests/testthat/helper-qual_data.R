f7ab12bbf4b79d4672838c7b2f333f2617a65769
Laura Maxwell <laura.maxwell@atorusresearch.com>
2026-04-16 08:33:48 -0400

    update lData in helper-qual_data.R

--- tests/testthat/helper-qual_data.R
+++ tests/testthat/helper-qual_data.R
@@ -42,7 +42,11 @@ lData <- list(
     rename(visit = foldername),
   Raw_IE = lSource$Raw_IE,
   Raw_VISIT = lSource$Raw_VISIT %>%
-    rename(visit = foldername)
+    rename(visit = foldername),
+  Raw_OverallResponse = lSource$Raw_OverallResponse %>%
+    rename(response_folder = foldername),
+  Raw_Death = lSource$Raw_Death,
+  Raw_Randomization = lSource$Raw_Randomization
 )
 
 ## Data with missing values (15% NA's)
