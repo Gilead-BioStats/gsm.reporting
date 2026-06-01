873e2f73bfdf4f23ad236ccaf4c7a6aa60e6f682
Jon Harmon <jon.harmon1@gilead.com>
2026-06-01 11:11:01 -0500

    Fix 62: MakeMetric passes fields
    Add tests to verify that MakeMetric passes through fields (specifically `Active` and `GenerateSignal`, but really they could be anything).
    
    I also added `Active: true` to exported workflows.
    
    - Closes #62

--- tests/testthat/test-MakeMetric.R
+++ tests/testthat/test-MakeMetric.R
@@ -1,3 +1,55 @@
+test_that("MakeMetric includes Active column when present in meta (#62)", {
+  given <- list(
+    a = list(
+      meta = list(
+        Type = "Metric",
+        ID = "kri0001",
+        GroupLevel = "Site",
+        Abbreviation = "AE",
+        Active = TRUE
+      )
+    ),
+    b = list(
+      meta = list(
+        Type = "Metric",
+        ID = "kri0002",
+        GroupLevel = "Site",
+        Abbreviation = "SAE",
+        Active = FALSE
+      )
+    )
+  )
+  result <- MakeMetric(given)
+  expect_true("Active" %in% names(result))
+  expect_identical(result$Active, c(TRUE, FALSE))
+})
+
+test_that("MakeMetric includes GenerateSignal column when present in meta (#62)", {
+  given <- list(
+    a = list(
+      meta = list(
+        Type = "Metric",
+        ID = "kri0001",
+        GroupLevel = "Site",
+        Abbreviation = "AE",
+        GenerateSignal = TRUE
+      )
+    ),
+    b = list(
+      meta = list(
+        Type = "Metric",
+        ID = "kri0002",
+        GroupLevel = "Site",
+        Abbreviation = "SAE",
+        GenerateSignal = FALSE
+      )
+    )
+  )
+  result <- MakeMetric(given)
+  expect_true("GenerateSignal" %in% names(result))
+  expect_identical(result$GenerateSignal, c(TRUE, FALSE))
+})
+
 test_that("MakeMetric makes dfMetrics", {
   given <- list(
     a = list(
